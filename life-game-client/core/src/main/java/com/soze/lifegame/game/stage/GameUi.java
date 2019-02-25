46fa164c840b427cb091847a461d2c701faa3d05
--global <piknypiec@gmail.com>
2019-02-25 21:29:04 +0100

    Simple message dialog - simplification

--- life-game-client/core/src/main/java/com/soze/lifegame/game/stage/GameUi.java
+++ life-game-client/core/src/main/java/com/soze/lifegame/game/stage/GameUi.java
@@ -1,7 +1,5 @@
 package com.soze.lifegame.game.stage;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.utils.Timer;
 import com.soze.r2d.Component;
 import com.soze.r2d.Element;
 import com.soze.r2d.R;
@@ -21,10 +19,7 @@ public class GameUi extends Component {
   
   private Element createMessageDialog() {
     UiState dialogProps = new UiState();
-//    dialogProps.set("title", getState().get("dialogTitle"));
     dialogProps.set("fillParent", true);
-    dialogProps.set("x", Gdx.graphics.getWidth() / 2f);
-    dialogProps.set("y", Gdx.graphics.getHeight() / 2f);
     dialogProps.set("show", getState().get("showDialog"));
     dialogProps.set("debug", true);
   
@@ -40,8 +35,5 @@ public class GameUi extends Component {
   public Element render() {
     
     return createMessageDialog();
-//    return R.createElement("TABLE", new UiState(), Arrays.asList(
-//      createMessageDialog()
-//    ));
   }
 }
