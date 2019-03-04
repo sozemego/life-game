f4b96766b8fdff887d9129a086c31856ed9f95e2
sozemego <piknypiec@gmail.com>
2019-03-04 21:45:37 +0100

    Add Game reducer to reducers

--- life-game-website-client/src/store/reducers.js
+++ life-game-website-client/src/store/reducers.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import user from '../user/reducer';
 import app from '../app/reducer';
+import { reducer as game } from '../game/reducer';
 
 export default combineReducers({
   app,
   user,
+  game,
 });
