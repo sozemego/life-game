a8d25199d5148b35c484e4c6581796a0449fd115
sozemego <piknypiec@gmail.com>
2019-03-04 20:22:29 +0100

    Add Button component
    This component passes all props down to <button> except for disabled and onClick.
    onClick is wrapped in a function. If the button is already in 'fetching' state, it becomes disabled (disabled = true).

---
+++ life-game-website-client/src/components/button/Button.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { LineProgress } from '../line-progress/LineProgress';
+
+
+export const Button = ({ children, onClick, disabled = false, ...props }) => {
+
+  const [fetching, setFetching] = useState(false);
+
+  const onClickDelegate = () => {
+    const result = onClick();
+    if (result instanceof Promise) {
+      setFetching(true);
+      result.then(() => setFetching(false));
+    }
+  };
+
+  disabled = disabled || fetching;
+
+  return (
+    <button {...props} onClick={onClickDelegate} disabled={disabled}>
+      {children}
+      <LineProgress isFetching={fetching}/>
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.any,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  content: null,
+  onClick: () => {
+  },
+};
