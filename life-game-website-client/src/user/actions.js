7bd64e54893f6b0df0ea353636f110e25cf81195
sozemego <piknypiec@gmail.com>
2019-03-07 21:33:26 +0100

    Set token after successful login

--- life-game-website-client/src/user/actions.js
+++ life-game-website-client/src/user/actions.js
@@ -1,11 +1,12 @@
 import { getLoginService } from '../api/ServiceFactory';
 import { history } from '../app/history';
 import { makePayloadActionCreator } from '../store/utils';
-import { SET_USER_NAME } from './action-types';
+import { SET_TOKEN, SET_USER_NAME } from './action-types';
 
 const loginService = getLoginService();
 
 export const setName = makePayloadActionCreator(SET_USER_NAME);
+export const setToken = makePayloadActionCreator(SET_TOKEN);
 
 export const register = (username, password) => {
   return (dispatch, getState) => {
@@ -17,7 +18,10 @@ export const register = (username, password) => {
 export const login = (username, password) => {
   return (dispatch, getState) => {
     return loginService.login(username, password)
-      .then(() => dispatch(setName(username)))
+      .then((token) => {
+        dispatch(setName(username));
+        dispatch(setToken(token));
+      })
       .then(() => setImmediate(() => history.push('/')));
   };
 };
