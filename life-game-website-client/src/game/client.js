115bed3c608aa2a1652d4a222b6515276763e3d4
sozemego <piknypiec@gmail.com>
2019-03-07 21:34:10 +0100

    Client can now send AuthorizeMessage

---
+++ life-game-website-client/src/game/client.js
@@ -0,0 +1,55 @@
+const uuid = require('uuid/v4');
+
+export const createGameClient = (user) => {
+  let socket = null;
+
+  const client = {};
+
+  client.connect = () => {
+    if (socket) {
+      return Promise.reject('Already connected or connecting');
+    }
+
+    socket = new WebSocket('ws://localhost:8000/game?what=what');
+
+    return new Promise((resolve, reject) => {
+      socket.onopen = (ws) => {
+        console.log('connected to game server');
+        resolve();
+      };
+
+      socket.onmessage = (message) => {
+        const parsed = JSON.parse(message.data);
+        console.log('received message from server ' + parsed.type);
+        // dispatch(parsed);
+      };
+
+      socket.onclose = () => {
+        console.log('connection closed');
+        // dispatch(logoutThunk());
+      };
+
+      socket.onerror = (e) => {
+        console.log(e);
+      };
+    });
+  };
+
+  client.send = (message) => {
+    socket.send(JSON.stringify(message));
+  };
+
+  client.authorize = () => {
+    const authMessage = {
+      username: user.name,
+      token: user.token,
+      messageId: uuid(),
+      type: 'AUTHORIZE'
+    };
+    client.send(authMessage);
+  };
+
+  return client;
+};
+
+
