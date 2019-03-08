e62254658177fdfa187006612ed1b5401ef83238
sozemego <piknypiec@gmail.com>
2019-03-08 19:44:26 +0100

    Add createGame action which starts game engine

--- life-game-website-client/src/game/actions.js
+++ life-game-website-client/src/game/actions.js
@@ -1,5 +1,7 @@
 import { makePayloadActionCreator } from '../store/utils';
-import { createGame } from './game';
+import { createGameClient } from './client';
+import { getUser } from '../user/selectors';
+import { createGame as gameFactory } from './game';
 
 export const GAME_STARTED = 'GAME_STARTED';
 export const setGameStarted = makePayloadActionCreator(GAME_STARTED);
@@ -7,10 +9,33 @@ export const setGameStarted = makePayloadActionCreator(GAME_STARTED);
 export const startGame = () => {
   return (dispatch, getState) => {
     console.log('STARTING GAME')
-    dispatch(setGameStarted(true));
 
-    const game = createGame();
+    const user = getUser(getState);
+    //1 create client
+    const client = createGameClient(user);
+    //2 connect client
+
+
+
+    return client.connect()
+      .then(() => {
+        //3 start the connect to game world flow
+        client.authorize();
+
+        client.onMessage((msg) => {
+          if (msg.type === 'AUTHORIZED') {
+            //4 pass connected client to game
+            dispatch(createGame(client));
+          }
+        });
+      });
+  };
+};
+
+export const createGame = (client) => {
+  return (dispatch, getState) => {
+    console.log('CREATING THE GAME ENGINE')
+    const game = gameFactory(client);
     game.start();
-
   };
 };
