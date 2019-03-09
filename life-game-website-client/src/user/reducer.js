b4423749c13cb3fe211b6b818696ab05b8345f6c
sozemego <piknypiec@gmail.com>
2019-03-09 21:51:48 +0100

    Save token and user name in local storage

--- life-game-website-client/src/user/reducer.js
+++ life-game-website-client/src/user/reducer.js
@@ -1,14 +1,32 @@
-import { createPayloadSetter, createReducer } from '../store/utils';
+import { createReducer } from '../store/utils';
 import { SET_TOKEN, SET_USER_NAME } from './action-types';
 
+const setName = (state, action) => {
+  localStorage.setItem('USER_NAME', action.payload);
+  return { ...state, name: action.payload };
+};
+
+const setToken = (state, action) => {
+  localStorage.setItem('TOKEN', action.payload);
+  return { ...state, token: action.payload };
+};
+
+const getName = () => {
+  return localStorage.getItem('USER_NAME') || null;
+};
+
+const getToken = () => {
+  return localStorage.getItem('TOKEN') || null;
+};
+
 const initialState = {
-  name: null,
-  token: null,
+  name: getName(),
+  token: getToken(),
 };
 
 const reducer = createReducer(initialState, {
-  [SET_USER_NAME]: createPayloadSetter('name'),
-  [SET_TOKEN]: createPayloadSetter('token'),
+  [SET_USER_NAME]: setName,
+  [SET_TOKEN]: setToken,
 });
 
 export default reducer;
