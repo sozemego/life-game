fb8773618d38110b8eeb1417b289c5c5641478f8
sozemego <piknypiec@gmail.com>
2019-03-09 21:37:58 +0100

    Add GameClient interface

--- life-game-website-client/src/game/index.d.ts
+++ life-game-website-client/src/game/index.d.ts
@@ -1,3 +1,11 @@
 export interface GameService {
     start(): Promise<any>
 }
+
+export interface GameClient {
+    connect(): Promise<any>
+    send(body: string): void
+    authorize(): void
+    requestGameWorld(): void
+    onMessage(type: string, fn: () => void): () => void
+}
