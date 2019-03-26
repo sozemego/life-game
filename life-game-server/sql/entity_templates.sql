1b78e52adeda902921a90676ecfade71a2fcca40
sozemego <piknypiec@gmail.com>
2019-03-26 20:42:07 +0100

    Set explicit width and height for some entity templates

--- life-game-server/sql/entity_templates.sql
+++ life-game-server/sql/entity_templates.sql
@@ -2,18 +2,22 @@ DELETE FROM ENTITY WHERE world_id = -1;
 
 INSERT INTO ENTITY (name, world_id, physics, graphics) VALUES
 (
- 'WAREHOUSE_1', -1,
- '{"type":  "PHYSICS"}', '{"type":  "GRAPHICS", "texture": "WAREHOUSE_1"}'
+  'WAREHOUSE_1', -1,
+  '{"type":  "PHYSICS", "width":  3, "height":  3}', '{"type":  "GRAPHICS", "texture": "WAREHOUSE_1"}'
 );
 
 INSERT INTO ENTITY (name, world_id, physics, graphics, resource_provider) VALUES
 (
- 'FOREST_1', -1,
- '{"type":  "PHYSICS"}', '{"type":  "GRAPHICS", "texture": "FOREST_1"}', '{"type": "RESOURCE_PROVIDER", "resource": "WOOD"}'
+  'FOREST_1', -1,
+  '{"type": "PHYSICS", "width":  3, "height":  3}',
+  '{"type": "GRAPHICS", "texture": "FOREST_1"}',
+  '{"type": "RESOURCE_PROVIDER", "resource": "WOOD"}'
 );
 
 INSERT INTO ENTITY (name, world_id, physics, graphics, harvester) VALUES
 (
   'WORKER_1', -1,
-  '{"type":  "PHYSICS"}', '{"type": "GRAPHICS", "texture": "WORKER_1"}', '{"type": "HARVESTER"}'
+  '{"type": "PHYSICS", "width":  1, "height":  1}',
+  '{"type": "GRAPHICS", "texture": "WORKER_1"}',
+  '{"type": "HARVESTER"}'
 );
