d06ecb35fea8c4b63e81f589472eaa1a6310ea06
sozemego <piknypiec@gmail.com>
2019-04-07 00:14:26 +0200

    When user orders entity to harvest, set targetEntityId on harvester component

--- life-game-website-client/src/game/game-engine/input/TargetEntityHandler.ts
+++ life-game-website-client/src/game/game-engine/input/TargetEntityHandler.ts
@@ -2,6 +2,7 @@ import { Mouse } from "../../InputHandler";
 import { GameEngine } from "../GameEngine";
 import { TYPES } from "../../ecs/component/types";
 import { EntityAction } from "../EntityAction";
+import { ResourceHarvesterComponent } from "../../ecs/component/FactoryRegistry";
 
 export const createTargetEntityHandler = (gameEngine: GameEngine) => {
   return (mouse: Mouse) => {
@@ -32,12 +33,13 @@ export const createTargetEntityHandler = (gameEngine: GameEngine) => {
 
     //here we have to handle different types of components that can target other entities
     //1. harvester
-    const harvester = currentSelectedEntity.getComponent(TYPES.HARVESTER);
+    const harvester = currentSelectedEntity.getComponent(TYPES.HARVESTER) as ResourceHarvesterComponent;
     if (harvester) {
 
       //1a. check if targeted entity is a resource provider
       const targetResourceProvider = clickedEntity.getComponent(TYPES.RESOURCE_PROVIDER);
       if (targetResourceProvider) {
+        harvester.targetEntityId = clickedEntity.id;
         gameEngine.targetEntity(currentSelectedEntity, clickedEntity, EntityAction.HARVEST);
       }
 
