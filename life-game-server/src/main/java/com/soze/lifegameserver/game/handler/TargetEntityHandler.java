00a2922d57b1f321a17717c869f4b8334f495715
sozemego <piknypiec@gmail.com>
2019-04-06 17:34:31 +0200

    TargetEntityHandler accepts a TargetEntityMessage (only logs it for now)

--- life-game-server/src/main/java/com/soze/lifegameserver/game/handler/TargetEntityHandler.java
+++ life-game-server/src/main/java/com/soze/lifegameserver/game/handler/TargetEntityHandler.java
@@ -1,5 +1,10 @@
 package com.soze.lifegameserver.game.handler;
 
+import com.soze.lifegame.common.ws.message.client.TargetEntity;
+import com.soze.lifegameserver.game.ws.GameSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,12 +13,20 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TargetEntityHandler {
-
-//  @EventListener(condition = "#event.clientMessageType.equals('REQUEST_WORLD')")
-//  public void handleTargetEntity(ClientMessageEvent event) {
-//    System.out.println(event);
-//    System.out.println("REQUEST_WORLD");
-//  }
-
-
+  
+  private static final Logger LOG = LoggerFactory.getLogger(TargetEntityHandler.class);
+  
+  @EventListener(condition = "#event.clientMessageType.equals('TARGET_ENTITY')")
+  public void handleTargetEntity(ClientMessageEvent event) {
+    handleTargetEntity(event.getSession(), (TargetEntity) event.getMessage());
+  }
+  
+  private void handleTargetEntity(GameSession session, TargetEntity message) {
+    LOG.info("User with session [{}] wants entity [{}] to target entity [{}]",
+             session.getSession().getId(), message.getSourceEntityId(), message.getTargetEntityId()
+    );
+    
+    
+  }
+  
 }
